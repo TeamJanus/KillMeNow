de430a0119f18746f83719c9a9495d2c1bd34e66
Stanislav Schaller <stas.moogle@gmail.com>
2018-06-06 17:45:37 -0400

    First post, unity style. Let's actually do this

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public static GameManager gm = null;
+
+    private void Awake() {
+        if (gm == null) gm = this;
+        else if (gm != this) Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager sm = null;
+
+    private void Awake() {
+        if (sm == null) sm = this;
+        else if (sm != this) Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
