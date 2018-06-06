aaaa2cb7d9f83f136c95c46e68986e41307d1db4
Stanislav Schaller <stas.moogle@gmail.com>
2018-06-06 19:54:33 -0400

    Added images and set background canvas

--- Assets/Scripts/SoundManager.cs
+++ Assets/Scripts/SoundManager.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour {
 
+    public AudioSource efxSource;
+    public AudioSource musicSource;
     public static SoundManager sm = null;
+
+    public float lowPitchRange = .95f;
+    public float highPitchRange = 1.05f;
 
     private void Awake() {
         if (sm == null) sm = this;
         else if (sm != this) Destroy(gameObject);
 
         DontDestroyOnLoad(gameObject);
+    }
+
+    public void PlaySingle(AudioClip clip) {
+        efxSource.clip = clip;
+        efxSource.Play();
+    }
+
+    public void RandomizeSfx(params AudioClip[] clips) {
+        int randomIndex = Random.Range(0, clips.Length);
+        float randomPitch = Random.Range(lowPitchRange, highPitchRange);
+
+        efxSource.pitch = randomPitch;
+        efxSource.clip = clips[randomIndex];
+        efxSource.Play();
     }
 }
