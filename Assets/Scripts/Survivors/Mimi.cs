ceb5541f2d004820fab0f0180467abf9f8c070e0
Stanislav Schaller <stas.moogle@gmail.com>
2018-06-10 17:16:26 -0400

    Created Survivor parent script and three implementations

---
+++ Assets/Scripts/Survivors/Mimi.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mimi : Survivor {
+
+    // Use this for initialization
+    void Start() {
+        stats.charName = "Mimi Necrosynth God Queen";
+        stats.loot = 70;
+        stats.combat = 100;
+        stats.build = 10;
+        stats.rally = 50;
+    }
+}
---
+++ Assets/Scripts/Survivors/Survivor.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Survivor : MonoBehaviour {
+
+    protected Stats stats;
+
+    // Use this for initialization
+    void Start () {
+        stats.charName = "Survivor";
+        stats.loot = 1;
+        stats.combat = 1;
+        stats.build = 1;
+        stats.rally = 1;
+
+        Debug.Log(stats);
+    }
+}
+
+// Pure data construct for holding the stats. Should range from 1 to 100.
+public struct Stats {
+    public string charName;
+    public int loot;
+    public int combat;
+    public int build;
+    public int rally;
+};
+
