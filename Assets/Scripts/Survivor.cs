dbf724f769b02f8c66e9322f270222030280b2bd
Stanislav Schaller <stas.moogle@gmail.com>
2018-06-13 22:48:21 -0400

    Created canvas for each survivor and proof of conceped showing and disappearing on mouseover

---
+++ Assets/Scripts/Survivor.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Survivor : MonoBehaviour, 
+    IPointerEnterHandler,
+    IPointerExitHandler{
+
+    protected SpriteRenderer sprite;
+    protected Transform slot;
+
+    public Canvas canvas;
+    public Text nameText;
+
+    public string charName = "Survivor";
+    public int loot = 1;
+    public int combat = 1;
+    public int build = 1;
+    public int rally = 1;
+
+    void Awake () {
+        nameText.text = this.charName;
+        nameText.enabled = false;
+    }
+
+    void Start() {
+        
+    }
+
+    void Update() {
+
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        nameText.enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        nameText.enabled = false;
+    }
+}
+
