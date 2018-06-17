b13e9767ca839ba157488f44e96c68cd8ace77c2
Stanislav Schaller <stas.moogle@gmail.com>
2018-06-16 20:57:43 -0400

    Added more reports to night panel. Implemented updating numbers and ui separately for nicer effect.

---
+++ Assets/Scripts/FadePanelAndText.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class FadePanelAndText : MonoBehaviour {
+
+    private const int FIRST_FADE_SPEED = 3;
+    private const int FADE_SPEED = 1;
+
+    private Image blackPanel;
+    private Text[] texts;
+    private EventTrigger trigger;
+
+    private void Awake() {
+        blackPanel = GetComponent<Image>();
+        texts = GetComponentsInChildren<Text>();
+        trigger = GetComponent<EventTrigger>();
+    }
+
+    // Fade out at start of game to inform difficulty
+    private void Start() {
+        FirstFadeOut();
+    }
+
+    // Adapted from Kiwasi Games's "Unity UI Fade Out" at https://youtu.be/MkoIZTFUego
+    public void FadeIn() {
+        gameObject.SetActive(true);
+        GameManager.gm.AdvanceDay();
+        StartCoroutine(DoFadeIn());
+    }
+
+    public void FadeOut() {
+        GameManager.gm.UpdateGUI();
+        StartCoroutine(DoFadeOut());
+        trigger.enabled = false;
+    }
+
+    private void FirstFadeOut() {
+        blackPanel.color = new Color(blackPanel.color.r, blackPanel.color.g, blackPanel.color.b, 1f);
+
+        foreach (Text text in texts) text.color = new Color(text.color.r, text.color.g, text.color.b, 1f);
+
+        gameObject.SetActive(true);
+        StartCoroutine(DoFirstFadeOut());
+    }
+
+    IEnumerator DoFadeIn() {
+        trigger.enabled = false;
+
+        Color alpha = blackPanel.color;
+        while (alpha.a < 1f) {
+            alpha.a += Time.deltaTime / FADE_SPEED;
+            blackPanel.color = alpha;
+
+            foreach (Text text in texts) {
+                Color textAlpha = text.color;
+                textAlpha.a += Time.deltaTime / FADE_SPEED;
+                text.color = textAlpha;
+            }
+
+            yield return null;
+        }
+        trigger.enabled = true;
+
+        yield return null;
+    }
+
+    IEnumerator DoFadeOut() { 
+        Color alpha = blackPanel.color;
+        while (alpha.a > 0f) {
+            alpha.a -= Time.deltaTime / FADE_SPEED;
+            blackPanel.color = alpha;
+
+            foreach (Text text in texts) {
+                Color textAlpha = text.color;
+                textAlpha.a -= Time.deltaTime / FADE_SPEED;
+                text.color = textAlpha;
+            }
+
+            yield return null;
+        }
+        gameObject.SetActive(false);
+
+        yield return null;
+    }
+
+    IEnumerator DoFirstFadeOut() {
+        trigger.enabled = false;
+
+        Color alpha = blackPanel.color;
+        while (alpha.a > 0f) {
+            alpha.a -= Time.deltaTime / FIRST_FADE_SPEED;
+            blackPanel.color = alpha;
+
+            foreach(Text text in texts) {
+                Color textAlpha = text.color;
+                textAlpha.a -= Time.deltaTime / FIRST_FADE_SPEED;
+                text.color = textAlpha;
+            }
+
+            yield return null;
+        }
+        gameObject.SetActive(false);
+        trigger.enabled = true;
+
+        yield return null;
+    }
+
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -18,11 +18,20 @@ public class GameManager : MonoBehaviour {
     public Text foodText;
     public Text zombieText;
 
+    public Text dayReport;
+    public Text actionReport;
+    public Text numbersReport;
+
     private int dayCount = 1;
     private int barrierCount = 100;
     private int foodCount = 100;
     private int zombieCount = 1;
     private int survivorCount = 1;
+
+    private int daysLeft = 10;
+    private string[] neutralDescriptors = new string[] { "The banging at the door grows louder tonight.",
+                                                         "The survivors grab a few hours of uneasy sleep.",
+                                                         "Night watch catches glimpses of strange shapes in the fading light" };
 
     void Awake() {
         if (gm == null) gm = this;
@@ -32,26 +41,29 @@ public class GameManager : MonoBehaviour {
     }
 
     void Start() {
-        updateGUI();
+        UpdateGUI();
     }
 
-    private void updateGUI() {
+    public void UpdateGUI() {
         dayText.text = dayCount.ToString();
         barrierText.text = barrierCount.ToString();
         foodText.text = foodCount.ToString();
         zombieText.text = zombieCount.ToString();
     }
 
-    public void advanceDay() {
-        
-    }
-
-    public void advanceGUI() {
+    public void AdvanceDay() {
         dayCount += 1;
         barrierCount -= ZOMBIE_DAMAGE * zombieCount;
         foodCount -= HUNGER_SCORE * survivorCount;
         zombieCount += 1;
 
-        updateGUI();
+        daysLeft -= 1;
+        dayReport.text = string.Format("{0} days left until help arrives.", daysLeft);
+
+        actionReport.text = neutralDescriptors[Random.Range(0, neutralDescriptors.Length)];
+
+        numbersReport.text = string.Format("Barrier took {0} damage. {1} units of food left. {2} horrors at the door.",
+                                            ZOMBIE_DAMAGE * zombieCount, foodCount, zombieCount);
     }
+
 }
