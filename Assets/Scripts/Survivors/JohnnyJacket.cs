eb780888a7d03c9daaeec0ad6c068ef08d3e7a70
Stanislav Schaller <stas.moogle@gmail.com>
2018-07-02 00:36:12 -0400

    Overrode TalkCanvasToggle using new JohnnyJacket script

--- Assets/Scripts/Survivors/JohnnyJacket.cs
+++ Assets/Scripts/Survivors/JohnnyJacket.cs
@@ -1,8 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class JohnnyJacket : Survivor {
+
+    private const string message = "What's up friendo? I'm Johnny Jacket";
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +16,14 @@ public class JohnnyJacket : Survivor {
 	void Update () {
 		
 	}
+
+    public new void TalkCanvasToggle() {
+        talkCanvas.gameObject.SetActive(true);
+
+        // TODO: the children get components seems to be deterministic but I'm using magic numbers here. Can I get this not so... guessy?
+        Image[] comps = talkCanvas.GetComponentsInChildren<Image>();
+        comps[1].GetComponentInChildren<Image>().sprite = portrait;
+        comps[3].GetComponentInChildren<Text>().text = charName;
+        comps[4].GetComponentInChildren<Text>().text = message;
+    }
 }
