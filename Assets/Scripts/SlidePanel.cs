3b33ac7e8a6094c3f777dfbaf804c54b1d28f712
Stanislav Schaller <stas.moogle@gmail.com>
2018-07-12 21:40:12 -0400

    Created Menu Canvas and animation to slide it in and out. Also reorganized some UI

---
+++ Assets/Scripts/SlidePanel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SlidePanel : MonoBehaviour {
+
+    public Image menuPanel;
+    public Animation slideAnimation;
+
+    private bool onScreen = false;
+
+    public void Slide() {
+        if (onScreen) {
+            slideAnimation.Play("MenuPanelSlideOut");
+            onScreen = false;
+        } else {
+            slideAnimation.Play("MenuPanelSlideIn");
+            onScreen = true;
+        }
+    }
+}
