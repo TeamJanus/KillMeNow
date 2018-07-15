54855235088c1058e880ac2d6daa7b6ccd91ffea
Stanislav Schaller <stas.moogle@gmail.com>
2018-07-15 19:13:17 -0400

    Implemented volume slider and saving in shared prefs

--- Assets/Scripts/PlayerPrefManager.cs
+++ Assets/Scripts/PlayerPrefManager.cs
@@ -1,61 +1,18 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-/** Taken from Michigan State University's Coursera-based 
-    Game Development And Development Specialization at https://www.coursera.org/specializations/game-development
-*/
 public static class PlayerPrefManager {
 
-    // Examples for later
-    public static int GetLives() {
-        if (PlayerPrefs.HasKey("Lives")) {
-            return PlayerPrefs.GetInt("Lives");
+    public static float GetVolume() {
+        if (PlayerPrefs.HasKey("Volume")) {
+            return PlayerPrefs.GetFloat("Volume");
         } else {
             return 0;
         }
     }
 
-    public static void SetLives(int lives) {
-        PlayerPrefs.SetInt("Lives", lives);
+    public static void SetVolume(float volume) {
+        PlayerPrefs.SetFloat("Volume", volume);
     }
 
-    public static int GetScore() {
-        if (PlayerPrefs.HasKey("Score")) {
-            return PlayerPrefs.GetInt("Score");
-        } else {
-            return 0;
-        }
-    }
-
-    public static void SetScore(int score) {
-        PlayerPrefs.SetInt("Score", score);
-    }
-
-    // story the current player state info into PlayerPrefs
-    public static void SavePlayerState(int score, int lives) { 
-        SetScore(score);
-        SetLives(lives);
-    }
-
-    // reset stored player state and variables back to defaults
-    public static void ResetPlayerState(int startLives) {
-        Debug.Log("Player State reset.");
-        SetScore(0);
-        SetLives(startLives);
-    }
-
-    // output the defined Player Prefs to the console
-    public static void ShowPlayerPrefs() {
-        // store the PlayerPref keys to output to the console
-        string[] values = { "Score", "Lives" };
-
-        // loop over the values and output to the console
-        foreach (string value in values) {
-            if (PlayerPrefs.HasKey(value)) {
-                Debug.Log(value + " = " + PlayerPrefs.GetInt(value));
-            } else {
-                Debug.Log(value + " is not set.");
-            }
-        }
-    }
 }
---
+++ Assets/Scripts/Settings.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Settings : MonoBehaviour {
+
+    public Slider volumeSlider;
+
+	// Use this for initialization
+	void Start () {
+        volumeSlider.value = PlayerPrefManager.GetVolume();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetVolume() {
+        PlayerPrefManager.SetVolume(volumeSlider.value);
+
+        SoundManager.sm.musicSource.volume = volumeSlider.value;
+    }
+}
